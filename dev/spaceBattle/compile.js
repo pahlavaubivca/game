eebe8cac4c93b8531b743d62c822ae4af3630220
pahlavaubivca <pahlavaubivca@gmail.com>
2016-09-11 20:44:47 +0300

    basic logic

--- dev/spaceBattle/compile.js
+++ dev/spaceBattle/compile.js
@@ -5,69 +5,119 @@ var compileObj = {};
 
 compileObj.init = function () {
     var canvas = document.getElementById("canvas");
-    var context = canvas.getContext("2d");
-    if(compileObj.defaults){
+    var field = canvas.getContext("2d");
+    compileObj.canvas = canvas;
+
+    if (compileObj.defaults) {
         var param = compileObj.defaults;
-        context.width = param.field.width+'%';
-        context.height = param.field.height+'%';
+
+        field.fillStyle = param.field.color;
+        field.fillRect(0, 0, param.field.width, param.field.height);
+        field.fill();
     }
 };
 compileObj.defaults = (function () {
     return {
         "field": {
-            "obstruction":{
-                "meteor":{}
+            "obstruction": {
+                "meteor": {}
             },
-            "width": 100,
-            "height": 100
+            "width": window.innerWidth,
+            "height": window.innerHeight,
+            "color": "black"
         },
         "unit": {
             "enemy": {},
             "nps": {},
             "GG": {
-                "size":{
-                    "width":50,
-                    "height":25,
-                    "weight":3
+                "physicalCharact": {
+                    "width": 50,
+                    "height": 25,
+                    "weight": 3,
+                    "position": {
+                        "x": 600,
+                        "y": 100
+                    },
+                    "leftM": false,
+                    "rightM": false,
+                    "topM": false,
+                    "downM": false,
+                    "fire": false
+
                 },
-                "level":{
-                    "life":3,
-                    "speed":1,
-                    "armor":1,
-                    "weapon":"",
-                    "shield":0,
-                    "hull":"standart"
+                "level": {
+                    "life": 3,
+                    "speed": 1,
+                    "armor": 1,
+                    "weapon": "",
+                    "shield": 0,
+                    "hull": "standart"
                 }
-            },
-            "hull":{
-                "standart":{},
-                "light":{},
-                "middle":{},
-                "heavy":{},
-                "terminator":{}
-            },
-            "weapon":{
-                "standart":{},
-                "atomatic":{},
-                "shotgun":{},
-                "epicShotgun":{},
-                "missile":{},
-                "laser":{},
-                "mrp":{},
-                "doom":{}
             }
 
+        },
+        "hull": {
+            "standart": {},
+            "light": {},
+            "middle": {},
+            "heavy": {},
+            "terminator": {}
+        },
+        "weapon": {
+            "standart": {},
+            "atomatic": {},
+            "shotgun": {},
+            "epicShotgun": {},
+            "missile": {},
+            "laser": {},
+            "mrp": {},
+            "doom": {}
         }
     }
 })();
 
-compileObj.GG = function(){
+compileObj.GG = function () {
+    var ship = compileObj.defaults.canvas.getContext("2d");
+    var ggParam = compileObj.defaults.unit.GG;
+    var ggPos = ggParam.physicalCharact;
+    ship.fillRect(ggPos.position.x, ggPos.position.y, ggPos.width, ggPos.height);
+};
+
+
+compileObj.registerAction = (function () {
+    var action = compileObj.defaults.unit.GG.physicalCharact;
+    window.addEventListener('keydown', function (event) {
+        event.keyCode == 37 ? action.leftM = true : action.leftM;
+        event.keyCode == 39 ? action.rightM = true : action.rightM;
+        event.keyCode == 87 ? action.topM = true : action.topM;
+        event.keyCode == 83 ? action.downM = true : action.downM;
+    });
+    window.addEventListener("click", function () {
+        action.fire = true;
+    });
+    window.addEventListener('keyUp', function (event) {
+        event.keyCode == 37 ? action.leftM = false : action.leftM;
+        event.keyCode == 39 ? action.rightM = false : action.rightM;
+        event.keyCode == 87 ? action.topM = false : action.topM;
+        event.keyCode == 83 ? action.downM = false : action.downM;
+    })
+})();
+
+compileObj.move = function () {
 
 };
-compileObj.draw = function(){
+
+compileObj.draw = function () {
 
 };
 compileObj.callMethods = (function () {
     compileObj.init();
-    //compileObj.GG();
+    for(var key in compileObj.unit){
+        if(compileObj.defaults.unit.hasOwnProperty(key)){
+            compileObj.defaults.unit[key]["context"] = compileObj.canvas.getContext('2d');
+        }
+    }
 })();
+window.requestAnimationFrame(function(event){
+   console.log(event)
+});
