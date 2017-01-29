bc40cfff91d38ba15251f9f1988bf87e4f332895
pahlavaubivca <pahlavaubivca@gmail.com>
2017-01-29 21:01:28 +0300

    insert into postgreqsl

--- dev/spaceBattle/server/handler/stat.js
+++ dev/spaceBattle/server/handler/stat.js
@@ -2,6 +2,20 @@
  * Created by pahlavaubivca on 19.11.2016.
  */
 var fs = require('fs');
+var pg = require('pg');
+var conString = "postgres://postgres:AdminUser1**@localhost:5432/spacebattle";
+var client = new pg.Client(conString);
+client.connect();
+
+/*var pool = new pg.Pool();
+var config = {
+    user:'postgres',
+    database: 'spacebattle',
+    password:'AdminUser1**',
+    host:'localhost',
+    port:5432
+};*/
+
 var fullUrlParser = /(https?:)?\/\/(.*?)\/(?:(.*)\/)?(\w+\.\w+)?[\#|\?]?(.*)?/i;
 var smallUrlParser = /\/(?:(.*)\/)?(.*)?[\?](.*)?/i;
 
@@ -12,6 +26,14 @@ module.exports = (function(){
         var urlComponent = smallUrlParser.exec(req.url)|| 'NO';
         if(urlComponent.length>3)urlComponent = urlComponent[3];
         res.end(urlComponent);
+        client.query('INSERT INTO speedstats (date,fps,encounter_time,jsonobj)' +
+            ' VALUES (current_date,1,1,$1)', [urlComponent], function(err, result) {
+            if(err) {
+                return console.error('error running query', err);
+            }
+            console.log(result.rows[0]);
+            //output: 1
+        });
         fs.writeFile("./tmp/test", "Hey there!", function(err) {
             if(err) {
                 return console.log(err);
